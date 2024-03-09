4eada6451ae6051ac2bfff227cfae7c7e33d7b9e
JasimAlibegovic <jalibegovic@echots.com>
2024-03-09 10:07:35 +0100

    login response dto

--- Iteach/IteachAPI/DTO/UserDTOs/LoginDTO.cs
+++ Iteach/IteachAPI/DTO/UserDTOs/LoginDTO.cs
@@ -4,3 +4,9 @@ public class LoginDTO
     public string Email { get;set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
 }
+public class LoginResponse
+{
+    public int Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public int Role { get; set; }    
+}
